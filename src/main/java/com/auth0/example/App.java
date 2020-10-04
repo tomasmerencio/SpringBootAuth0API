b7187669ae4ad4349aa138a5d8100a674f4db577
Tomás Merencio <tomasmerencio@gmail.com>
2020-10-04 01:22:02 -0300

    Agregar OpenAPI/Swagger

--- src/main/java/com/auth0/example/App.java
+++ src/main/java/com/auth0/example/App.java
@@ -1,7 +1,11 @@
 package com.auth0.example;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Main entry point of application.
@@ -10,5 +14,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                .title("User REST API")
+                .version("1.0.0")
+                .description("description")
+                .termsOfService("http://swagger.io/terms/"));
     }
 }
