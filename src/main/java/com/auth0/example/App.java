c3102fb4427cad22889a4d102eb4e430bfe069f8
Tomás Merencio <tomasmerencio@gmail.com>
2020-10-09 18:32:19 -0300

    Solucionar Error de Header Authorization en Swagger-UI

--- src/main/java/com/auth0/example/App.java
+++ src/main/java/com/auth0/example/App.java
@@ -1,8 +1,9 @@
 package com.auth0.example;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -23,6 +24,12 @@ public class App {
                 .title("User REST API")
                 .version("1.0.0")
                 .description("description")
-                .termsOfService("http://swagger.io/terms/"));
+                .termsOfService("http://swagger.io/terms/"))
+                .components(new Components()
+                .addSecuritySchemes("bearer-key",
+                new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")));
     }
 }
